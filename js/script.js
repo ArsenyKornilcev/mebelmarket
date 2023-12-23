5c681b36a8bf09f48998c7861a9eba3339e2888d
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2023-12-23 17:37:12 +0500

    location-menu 1920, js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-const up_btn_el = document.querySelector('footer .up');
+const up_btn_el = document.querySelector("footer .up");
+const location_elements = document.querySelectorAll("header .location");
 
-
-up_btn_el.addEventListener('click', () => {
-    document.querySelector('header').scrollIntoView(true, {
-        behavior: 'smooth',
-    });
+location_elements.forEach((el) => {
+	el.addEventListener("click", () => {
+        const loc_menu = el.querySelector(".location-menu");
+		loc_menu.classList.toggle("active");
+	});
 });
 
-
+up_btn_el.addEventListener("click", () => {
+	document.querySelector("header").scrollIntoView(true, {
+		behavior: "smooth",
+	});
+});
