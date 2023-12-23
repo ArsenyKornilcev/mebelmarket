53fd88793152d3aea27344592528db5c9eae2d4c
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2023-12-23 18:05:47 +0500

    header search fix, js for search 540

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const up_btn_el = document.querySelector("footer .up");
+
 const location_elements = document.querySelectorAll("header .location");
+
+const search_btn = document.querySelector('.header__menu .search button');
+const search_container = document.querySelector(".header__menu .right_group");
+
+search_btn.addEventListener('click', () => {
+    search_container.classList.toggle("right_group_search");
+})
 
 location_elements.forEach((el) => {
 	el.addEventListener("click", () => {
