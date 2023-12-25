00dc374ef8e9e213d8e903034aaf80161f23f55b
ArsenyKornilcev <sienia.korniltsiev@yandex.ru>
2023-12-25 18:38:50 +0500

    cart_mini js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-const up_btn_el = document.querySelector("footer .up");
+const up_btn__el = document.querySelector("footer .up");
 
-const location_elements = document.querySelectorAll("header .location");
+const location__elements = document.querySelectorAll("header .location");
 
-const search_btn = document.querySelector('.header__menu .search button');
-const search_container = document.querySelector(".header__menu .right_group");
+const search__btn = document.querySelector(".header__menu .search button");
+const search__container = document.querySelector(".header__menu .right_group");
 
-search_btn.addEventListener('click', () => {
-    search_container.classList.toggle("right_group_search");
-})
+const cart__icon = document.querySelector(".header__menu #cart");
+const cart_mini__panel = document.querySelector(
+	".header__menu #cart .cart_mini"
+);
 
-location_elements.forEach((el) => {
+cart__icon.addEventListener("click", () => {
+	cart_mini__panel.classList.toggle("active");
+});
+
+search__btn.addEventListener("click", () => {
+	search__container.classList.toggle("right_group_search");
+});
+
+location__elements.forEach((el) => {
 	el.addEventListener("click", () => {
-        const loc_menu = el.querySelector(".location-menu");
+		const loc_menu = el.querySelector(".location-menu");
 		loc_menu.classList.toggle("active");
 	});
 });
 
-up_btn_el.addEventListener("click", () => {
+up_btn__el.addEventListener("click", () => {
 	document.querySelector("header").scrollIntoView(true, {
 		behavior: "smooth",
 	});
